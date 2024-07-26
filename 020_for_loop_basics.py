a4ff916bffc587a5849df4e370fab40a78d32a6d
raduainrimon <raduainrimon.education@gmail.com>
2024-07-27 00:41:15 +0600

    for loop
    for loop practice

---
+++ 020_for_loop_basics.py
@@ -0,0 +1,18 @@
+'''Watch YouTube again'''
+# for item in 'python':
+#     print(item)
+
+# for item in ['Rimon','Emon','Sabbir']:
+#     print(item)
+
+# for item in ['1','2','3']:
+#     print(item)
+
+# for item in range(10):
+#     print(item)
+
+# for item in range(7, 10):
+#     print(item)
+
+for item in range(3, 10, 3):
+    print(item)
